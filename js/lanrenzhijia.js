546655c27409639bc1244ebb8f1e4865f342ce96
zzsah <zhengsheng.zhou@hp.com>
2017-01-30 16:15:08 +0800

    add

---
+++ js/lanrenzhijia.js
@@ -0,0 +1,33 @@
+/* ´úÂëÕûÀí£ºÀÁÈËÖ®¼Ò www.lanrenzhijia.com */
+$(document).ready(function(){
+
+
+  var playlist = [{
+      title:"童年",
+      artist:"shasha",
+      mp3:"audios/tongnian.m4a",
+      //oga:"http://www.jplayer.org/audio/ogg/Miaow-02-Hidden.ogg",
+      poster: "http://33.media.tumblr.com/0b35eb42176eedbf4a96e52efa760875/tumblr_mxp7a0v3fr1rqx86wo1_500.png"
+    },{
+      title:"童年",
+      artist:"shasha",
+      mp3:"../audio/tongnian.m4a",
+      //oga:"http://www.jplayer.org/audio/ogg/Miaow-02-Hidden.ogg",
+      poster: "http://33.media.tumblr.com/0b35eb42176eedbf4a96e52efa760875/tumblr_mxp7a0v3fr1rqx86wo1_500.png"
+    },
+
+    ];
+  
+  var cssSelector = {
+    jPlayer: "#jquery_jplayer",
+    cssSelectorAncestor: ".music-player"
+  };
+  
+  var options = {
+    swfPath: "http://cdnjs.cloudflare.com/ajax/libs/jplayer/2.6.4/jquery.jplayer/Jplayer.swf",
+    supplied: "ogv, m4v, oga, mp3"
+  };
+  
+  var myPlaylist = new jPlayerPlaylist(cssSelector, playlist, options);
+  
+});
