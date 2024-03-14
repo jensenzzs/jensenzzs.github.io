c6c93e4f252165f4fee72d4ef78b5ef9412fac8d
jensonzzs <jensen@gmail.com>
2024-03-14 23:42:32 +0800

    icon

--- vite.config.ts
+++ vite.config.ts
@@ -28,17 +28,27 @@ export default defineConfig({
         // background_color: "#373737",
         icons: [
           {
-            src: "logo.svg",
-            sizes: "512x512",
-            type: "image/svg+xml",
-            purpose: "any",
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
           },
-          // {
-          //   src: "appicon-apple.png",
-          //   sizes: "512x512",
-          //   type: "image/png",
-          //   purpose: "any",
-          // },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
         ],
       },
     })
